81dd162dbd6ecb2f801107a5bb119e3edd2f2552
jose-marcleiton <j.m.a.m7@hotmail.com>
2026-05-06 09:01:41 -0300

    ajuste na aula 14

--- AULA14/script.js
+++ AULA14/script.js
@@ -1,31 +1,31 @@
-//let desconto = 0
-//let cor = prompt('Digite a cor da etiqueta do produto: \n Verde \n Amarelo \n Azul \n Vermelho')
-//let valor = Number(prompt('Digite o valor do produto'))
-//let novoPreco = 0
+let desconto = 0
+let cor = prompt('Digite a cor da etiqueta do produto: \n Verde \n Amarelo \n Azul \n Vermelho')
+let valor = Number(prompt('Digite o valor do produto'))
+let novoPreco = 0
 
-//cor = cor.toLowerCase() // Converte a cor para minúsculo
+cor = cor.toLowerCase() // Converte a cor para minúsculo
 
-//number()
-//console.log()
+number()
+console.log()
 
-//switch (corEtiqueta) {
-   // case 'Verde':
-        //desconto = 0.1;  10%
-     //   break;
-    //case 'Amarelo':
-        //desconto = 0.2; 20%
-      //  break
-    //case 'Azul':
-      //  desconto = 0.3;  30%
-       // break
-    //case 'Vermelho':
-      //  desconto = 0.4;  40%
-        //break
-    //default:
-       // console.log('Cor de etiqueta inválida');
-        //desconto= 0  Retorna o preço sem desconto se a cor da etiqueta for inválida
-//}
+switch (corEtiqueta) {
+    case 'Verde':
+        desconto = 0.1;  10%
+        break;
+    case 'Amarelo':
+        desconto = 0.2; 20%
+        break
+    case 'Azul':
+        desconto = 0.3;  30%
+        break
+    case 'Vermelho':
+        desconto = 0.4;  40%
+        break
+    default:
+        console.log('Cor de etiqueta inválida');
+        desconto= 0  Retorna o preço sem desconto se a cor da etiqueta for inválida
+}
 
-//novoPreco = valor * (1 - desconto)
-//console.log(`O valor do produto com a etiqueta ${cor} teve o valor de R$ ${valor} ajustado para ${novoPreco} `) 
+novoPreco = valor * (1 - desconto)
+console.log(`O valor do produto com a etiqueta ${cor} teve o valor de R$ ${valor} ajustado para ${novoPreco} `) 
 
