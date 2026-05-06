d30ffdf55ac0e879c128e5596541124dd04cf246
jose-marcleiton <j.m.a.m7@hotmail.com>
2026-05-06 08:57:03 -0300

    ajuste na aula 13

--- AULA13/index.js
+++ AULA13/index.js
@@ -1,64 +1,64 @@
 
 
 
-// let numero = 2
+let numero = 2
 
-// if(numero ==0){
-//     console.log('zero')
-// }
-// else if (numero ==1){
-//     console.log('dois')
-// }
-// else{
-//     console.log('numero desconhecido')
-// }
+if(numero ==0){
+console.log('zero')
+}
+else if (numero ==1){
+console.log('um')
+}
+else{
+console.log('numero desconhecido')
+}
 
-// atividade de aula
+//atividade de aula
 
-// let num1 = Number(prompt('digite o primeiro numero'))
-// let num2 = Number(prompt('digite o segundo numero'))
+ let num1 = Number(prompt('digite o primeiro numero'))
+ let num2 = Number(prompt('digite o segundo numero'))
 
-// if(num1 > num2){
-// alert('o primeiro numero é maior que o segundo')
-// }
+if(num1 > num2){
+alert('o primeiro numero é maior que o segundo')
+}
 
 
-// else if(num2 > num1){
-//     alert('o segundo numero é maior que o primeiro')
-// }
+else if(num2 > num1){
+alert('o segundo numero é maior que o primeiro')
+}
 
-// else{
-// alert('os numero são iguais')
-// }
+else{
+alert('os numero são iguais')
+}
 
 // condição de paridade
 
-// let num = 7.2
-// console.log(num / 2)
-// console.log(num % 2)
-// console.log((num % 2) == 0)
+let num = 7.2
+console.log(num / 2)
+console.log(num % 2)
+console.log((num % 2) == 0)
 
-// if(num % 2 == 0){
-//     console.log('par')
-// }
+if(num % 2 == 0){
+ console.log('par')
+}
 
-// else {
-//     console.log ('impar')
-// }
+else {
+ console.log ('impar')
+}
 
-// console.log(Number('palavra'))
+console.log(Number('palavra'))
 
-// let num = Number(prompt('digite um numero'))
+let num = Number(prompt('digite um numero'))
 
-// if(isNaN(num)){
-//     console.log('digite apenas numeros')
-// }
-// else if (num%2 == 0){
-//     num = num + 5
-//     console.log (num)
-// }
+if(isNaN(num)){
+console.log('digite apenas numeros')
+}
+else if (num%2 == 0){
+num = num + 5
+console.log (num)
+}
 
-// else {
-//     num = num + 8
-//     console.log = (num)
-// }
+else {
+ num = num + 8
+console.log = (num)
+}
